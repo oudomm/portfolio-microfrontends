a7c9b93b9cea6a72ca161f46eaf8336838036004
oudom <oudomphoem@gmail.com>
2026-02-10 20:14:52 +0700

    refactor: add each apps vercel url

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    federation({
-      name: "shell",
-      remotes: {
-        home: "http://localhost:3001/assets/remoteEntry.js",
-        projects: "http://localhost:3002/assets/remoteEntry.js",
-        blog: "http://localhost:3003/assets/remoteEntry.js",
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
 
-      shared: ["react", "react-dom"],
-    }),
-    react(),
-  ],
-  build: {
-    target: "esnext",
-  },
-  server: { port: 3000, strictPort: true },
-  preview: { port: 5000, strictPort: true },
+  const homeBase = env.VITE_HOME_URL || "http://localhost:3001";
+  const projectsBase = env.VITE_PROJECTS_URL || "http://localhost:3002";
+  const blogBase = env.VITE_BLOG_URL || "http://localhost:3003";
+
+  return {
+    plugins: [
+      tailwindcss(),
+      federation({
+        name: "shell",
+        remotes: {
+          home: `${homeBase}/assets/remoteEntry.js`,
+          projects: `${projectsBase}/assets/remoteEntry.js`,
+          blog: `${blogBase}/assets/remoteEntry.js`,
+        },
+        shared: ["react", "react-dom"],
+      }),
+      react(),
+    ],
+    build: { target: "esnext" },
+    server: { port: 3000, strictPort: true },
+    preview: { port: 5000, strictPort: true },
+  };
 });
